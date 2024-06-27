d9cd5ee27b0d0818005a3c0db82b20dc74ade2b0
MilanBhanuka <vgmbhanuka@gmail.com>
2024-06-27 23:47:44 +0530

    nav bar , footer

--- metanet/src/app/app.component.ts
+++ metanet/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import{LandingnavbarComponent} from './landingnavbar/landingnavbar.component';
+import{ HomepageComponent } from './homepage/homepage.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingnavbarComponent],
+  imports: [RouterOutlet, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- metanet/src/app/homepage/homepage.component.ts
+++ metanet/src/app/homepage/homepage.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { LandingnavbarComponent } from '../landingnavbar/landingnavbar.component';
+import { FooterComponent } from '../footer/footer.component';
+
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [LandingnavbarComponent,FooterComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
