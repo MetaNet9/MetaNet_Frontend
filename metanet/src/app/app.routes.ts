b2c8ff29a0b817a28b743393abc878612c23cd1c
MilanBhanuka <vgmbhanuka@gmail.com>
2024-07-02 17:42:58 +0530

    contactus

--- metanet/src/app/app.routes.ts
+++ metanet/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ServicesComponent } from './services/services.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 export const routes: Routes = [
   {
@@ -19,6 +20,10 @@ export const routes: Routes = [
   {
     path: 'services',
     component: ServicesComponent
+  },
+  {
+    path: 'contactus',
+    component : ContactusComponent
   }
 
 ];
