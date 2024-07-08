0b49ef4f6412f1c2de93a23449c66102dc66b1ae
venuri <2021is003@stu.ucsc.cmb.ac.lk>
2024-07-08 19:39:29 +0530

    Basic reg success

--- metanet/src/app/usercomponents/registrationsuccess/registrationsuccess.component.ts
+++ metanet/src/app/usercomponents/registrationsuccess/registrationsuccess.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LandingnavbarComponent } from '../landingnavbar/landingnavbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-registrationsuccess',
