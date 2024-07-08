e0749813e12ef5fb8c3b15b48ca041f9e471f6aa
venuri <2021is003@stu.ucsc.cmb.ac.lk>
2024-07-08 19:36:02 +0530

    Basic reg success

---
+++ metanet/src/app/registrationsuccess/registrationsuccess.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrationsuccess',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrationsuccess.component.html',
+  styleUrl: './registrationsuccess.component.css'
+})
+export class RegistrationsuccessComponent {
+
+}
