3fed3e12ac28eaf635314a84c7485dc258882afc
venuri <2021is003@stu.ucsc.cmb.ac.lk>
2024-07-15 13:12:35 +0530

    Marketplace checkout basic

---
+++ metanet/src/app/usercomponents/marketplace-checkout/marketplace-checkout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marketplace-checkout',
+  standalone: true,
+  imports: [],
+  templateUrl: './marketplace-checkout.component.html',
+  styleUrl: './marketplace-checkout.component.css'
+})
+export class MarketplaceCheckoutComponent {
+
+}
