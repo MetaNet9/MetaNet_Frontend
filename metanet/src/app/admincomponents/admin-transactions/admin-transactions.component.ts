23c065005863d7886861752005e2b7f3f75dae24
isuru-liyanage <isuruliyanageofficial@gmail.com>
2024-07-28 12:14:00 +0530

    transaction page updated with tables

--- metanet/src/app/admincomponents/admin-transactions/admin-transactions.component.ts
+++ metanet/src/app/admincomponents/admin-transactions/admin-transactions.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
 import { AdminnavbarComponent } from 'src/app/commonComponents/adminnavbar/adminnavbar.component';
+import {AdminSidebarComponent} from "../../commonComponents/admin-sidebar/admin-sidebar.component";
+import {TableAdmin4ColoumComponent} from "../../commonComponents/table-admin-4-coloum/table-admin-4-coloum.component";
 
 @Component({
   selector: 'app-admin-transactions',
   standalone: true,
   imports: [
     AdminnavbarComponent,
-    CalendarModule, FormsModule
+    CalendarModule, FormsModule, AdminSidebarComponent, TableAdmin4ColoumComponent
   ],
   templateUrl: './admin-transactions.component.html',
   styleUrl: './admin-transactions.component.css'
