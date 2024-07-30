68cf3cddd06e65013b358123ace4d2587115f873
isuru-liyanage <isuruliyanageofficial@gmail.com>
2024-07-30 21:40:42 +0530

    moderator sidebar created

---
+++ metanet/src/app/commonComponents/moderator-sidebar/moderator-sidebar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-moderator-sidebar',
+  standalone: true,
+    imports: [
+        NgIf,
+        RouterLink
+    ],
+  templateUrl: './moderator-sidebar.component.html',
+  styleUrl: './moderator-sidebar.component.css'
+})
+export class ModeratorSidebarComponent {
+
+}
