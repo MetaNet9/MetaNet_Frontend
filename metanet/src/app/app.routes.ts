4cda1b5752214db8902fb0868beb237dfc72203a
MilanBhanuka <vgmbhanuka@gmail.com>
2024-07-30 20:18:44 +0530

    category page

--- metanet/src/app/app.routes.ts
+++ metanet/src/app/app.routes.ts
@@ -20,6 +20,7 @@ import {AdminSysAdminComponent} from "./admincomponents/admin-sys-admin/admin-sy
 import {AdminModelsComponent} from "./admincomponents/admin-models/admin-models.component";
 import { SettingsComponent } from './usercomponents/settings/settings.component';
 import { MarketplaceProductsComponent } from './usercomponents/marketplace-products/marketplace-products.component';
+import { MarketplaceCategoriesComponent } from './usercomponents/marketplace-categories/marketplace-categories.component';
 
 
 
@@ -32,6 +33,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'registrationsuccess', component: RegistrationsuccessComponent},
   { path: 'ordersuccessful', component: OrdersuccessfulComponent},
+  { path: 'marketplace-categories', component: MarketplaceCategoriesComponent},
   { path: 'marketplace-products', component: MarketplaceProductsComponent},
   { path: 'marketplace-checkout', component: MarketplaceCheckoutComponent},
   { path: 'marketplace-shoppingcart', component: MarketplaceShoppingcartComponent},
@@ -46,9 +48,10 @@ export const routes: Routes = [
   { path: 'admindashboard', component: AdminDashboardComponent},
   { path: 'admintransactions', component: AdminTransactionsComponent},
   { path: 'adminrevenue', component: AdminRevenueComponent},
-  {path: 'adminpayouts', component: AdminPayoutsComponent},
-  {path: 'adminsystem_admin', component: AdminSysAdminComponent},
-  {path: 'adminmodel', component: AdminModelsComponent}
+  { path: 'adminpayouts', component: AdminPayoutsComponent},
+  { path: 'adminsystem_admin', component: AdminSysAdminComponent},
+  { path: 'adminmodel', component: AdminModelsComponent},
+
 
 
 
--- metanet/src/app/usercomponents/user-profile/user-profile.component.ts
+++ metanet/src/app/usercomponents/user-profile/user-profile.component.ts
@@ -25,8 +25,8 @@ interface ModelPageEvent {
 }
 
 interface CollectionPageEvent{
-  collectionFirst?: number;
-  collectionRow?: number;
+  first?: number;
+  rows?: number;
   page?: number;
   pageCount?: number;
 }
@@ -89,14 +89,20 @@ export class UserProfileComponent {
 
 
   collectionFirst: number = 0;
-  collectionRow: number = 5;
+  collectionRow: number = 10;
   collections: Collection[] = [
-    { image: 'https://media.sketchfab.com/models/dda567d1ffbd40eabbce7625a48a39ef/thumbnails/84f3614735f048b5838b45f5eb5aa430/01ec6878522a49bc956f9ba4b9fe08f8.jpeg', name: 'Elephant', icon: 'pi pi-image', modelcount: 2 },
-    { image: 'https://media.sketchfab.com/models/73bf025d4a51401caa36c3e5950b4e65/thumbnails/81416d40064344ffb054042ae09194a9/822364e76c7642579e5831a8744016b5.jpeg', name: 'Tiger', icon: 'pi pi-image' , modelcount: 2},
-    { image: 'https://media.sketchfab.com/models/5e89e6689c134d54860e38eb35761ffe/thumbnails/d71ad53ff8dd48f084b667f77b90847c/56b5db8e8e7b46f1b41b9dc6355d06e9.jpeg', name: 'Home', icon: 'pi pi-image', modelcount: 2  },
-    { image: 'https://media.sketchfab.com/models/dda567d1ffbd40eabbce7625a48a39ef/thumbnails/84f3614735f048b5838b45f5eb5aa430/01ec6878522a49bc956f9ba4b9fe08f8.jpeg', name: 'Elephant', icon: 'pi pi-image', modelcount: 2  },
-    { image: 'https://media.sketchfab.com/models/73bf025d4a51401caa36c3e5950b4e65/thumbnails/81416d40064344ffb054042ae09194a9/822364e76c7642579e5831a8744016b5.jpeg', name: 'Tiger', icon: 'pi pi-image', modelcount: 2  },
-    { image: 'https://media.sketchfab.com/models/5e89e6689c134d54860e38eb35761ffe/thumbnails/d71ad53ff8dd48f084b667f77b90847c/56b5db8e8e7b46f1b41b9dc6355d06e9.jpeg', name: 'Home', icon: 'pi pi-image' },
+    { image: 'https://media.sketchfab.com/models/dda567d1ffbd40eabbce7625a48a39ef/thumbnails/84f3614735f048b5838b45f5eb5aa430/01ec6878522a49bc956f9ba4b9fe08f8.jpeg', name: 'Elephant', icon: 'pi-image', modelcount: 2 },
+    { image: 'https://media.sketchfab.com/models/73bf025d4a51401caa36c3e5950b4e65/thumbnails/81416d40064344ffb054042ae09194a9/822364e76c7642579e5831a8744016b5.jpeg', name: 'Tiger', icon: 'pi-image' , modelcount: 2},
+    { image: 'https://media.sketchfab.com/models/5e89e6689c134d54860e38eb35761ffe/thumbnails/d71ad53ff8dd48f084b667f77b90847c/56b5db8e8e7b46f1b41b9dc6355d06e9.jpeg', name: 'Home', icon: 'pi-image', modelcount: 2  },
+    { image: 'https://media.sketchfab.com/models/dda567d1ffbd40eabbce7625a48a39ef/thumbnails/84f3614735f048b5838b45f5eb5aa430/01ec6878522a49bc956f9ba4b9fe08f8.jpeg', name: 'Elephant', icon: 'pi-image', modelcount: 2  },
+    { image: 'https://media.sketchfab.com/models/73bf025d4a51401caa36c3e5950b4e65/thumbnails/81416d40064344ffb054042ae09194a9/822364e76c7642579e5831a8744016b5.jpeg', name: 'Tiger', icon: 'pi-image', modelcount: 2  },
+    { image: 'https://media.sketchfab.com/models/5e89e6689c134d54860e38eb35761ffe/thumbnails/d71ad53ff8dd48f084b667f77b90847c/56b5db8e8e7b46f1b41b9dc6355d06e9.jpeg', name: 'Home', icon: 'pi-image' , modelcount: 2 },
+    { image: 'https://media.sketchfab.com/models/dda567d1ffbd40eabbce7625a48a39ef/thumbnails/84f3614735f048b5838b45f5eb5aa430/01ec6878522a49bc956f9ba4b9fe08f8.jpeg', name: 'Elephant', icon: 'pi-image', modelcount: 2 },
+    { image: 'https://media.sketchfab.com/models/73bf025d4a51401caa36c3e5950b4e65/thumbnails/81416d40064344ffb054042ae09194a9/822364e76c7642579e5831a8744016b5.jpeg', name: 'Tiger', icon: 'pi-image' , modelcount: 2},
+    { image: 'https://media.sketchfab.com/models/5e89e6689c134d54860e38eb35761ffe/thumbnails/d71ad53ff8dd48f084b667f77b90847c/56b5db8e8e7b46f1b41b9dc6355d06e9.jpeg', name: 'Home', icon: 'pi-image', modelcount: 2  },
+    { image: 'https://media.sketchfab.com/models/dda567d1ffbd40eabbce7625a48a39ef/thumbnails/84f3614735f048b5838b45f5eb5aa430/01ec6878522a49bc956f9ba4b9fe08f8.jpeg', name: 'Elephant', icon: 'pi-image', modelcount: 2  },
+    { image: 'https://media.sketchfab.com/models/73bf025d4a51401caa36c3e5950b4e65/thumbnails/81416d40064344ffb054042ae09194a9/822364e76c7642579e5831a8744016b5.jpeg', name: 'Tiger', icon: 'pi-image', modelcount: 2  },
+    { image: 'https://media.sketchfab.com/models/5e89e6689c134d54860e38eb35761ffe/thumbnails/d71ad53ff8dd48f084b667f77b90847c/56b5db8e8e7b46f1b41b9dc6355d06e9.jpeg', name: 'Home', icon: 'pi-image' , modelcount: 2 },
     // Add more collections here
   ];
   paginatedCollections: Collection[] = [];
@@ -124,8 +130,8 @@ export class UserProfileComponent {
 
 // collections pagination
   onCollectionPageChange(event: CollectionPageEvent) {
-    this.first = event.collectionFirst || 0;
-    this.rows = event.collectionRow || 5;
+    this.collectionFirst = event.first || 0;
+    this.collectionRow = event.rows || 10;
     this.updatePaginatedCollections();
   }
 
