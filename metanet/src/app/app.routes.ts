fcf9bb483e86cc8aa54408998c02b43c0b4cf17e
Ravindu Nanayakkara <ravindupasannanayakkara@gmail.com>
2024-11-26 14:42:26 +0530

    become creator page added

--- metanet/src/app/app.routes.ts
+++ metanet/src/app/app.routes.ts
@@ -35,6 +35,7 @@ import { MarketplaceCategoriesComponent } from './usercomponents/marketplace-cat
 import { UploadFormComponent } from './usercomponents/upload-form/upload-form.component';
 import { ForgotPasswordComponent } from './usercomponents/forgot-password/forgot-password.component';
 import { NewPasswordComponent } from './usercomponents/new-password/new-password.component';
+import { BecomeCreatorComponent } from './usercomponents/become-creator/become-creator.component';
 
 
 
@@ -61,6 +62,7 @@ export const routes: Routes = [
   { path: 'upload-form', component: UploadFormComponent},
   { path: 'forgot-password', component: ForgotPasswordComponent},
   { path: 'new-password', component: NewPasswordComponent },
+  { path: 'become-creator', component: BecomeCreatorComponent},
   
 
 
