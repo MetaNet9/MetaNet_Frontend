ec7323e04f708e472748d2c1b3508a05787bf348
Isuru Liyanage <126383627+isuru-liyanage@users.noreply.github.com>
2024-12-01 19:15:14 +0530

    moderator creators list connected to Backend

--- metanet/src/app/domain/models.ts
+++ metanet/src/app/domain/models.ts
@@ -45,13 +45,32 @@ export interface model {
   image?: string;
   format?: string;
   price?: number;
-  reviews?: number;
+  review?: number;
+}
+
+export interface modelAdvanced{
+    id?: number;
+    title?: string;
+    description?: string;
+    modelUrl?: string;
+    image1Url?: string;
+    image2Url?: string;
+    image3Url?: string;
+    tags?: string[];
+    downloadType?: string;
+    license?: string;
+    format?: string;
+    price?: number;
+    downloads?: number;
+    likes?: number;
+    createdAt?: string;
+    review?: number;
 }
 
 
 
 export interface modelResponse {
-  data: model[];
+  data: modelAdvanced[];
 }
 
 interface revenue {categoryName:string, totalRevenue:string}
@@ -62,18 +81,53 @@ export interface Statistics {
   totalPurchases?: number;
   totalLikes?: string;
   totalDownloads?: string;
-  mostPopularModel?: model;
+  mostPopularModel?: modelAdvanced;
   lastMonthRevenue: revenue[];
   lastWeekRevenue?: any[];
   newModelsLastWeek?: number;
   newUsersLastWeek?: number;
+  moderatorsCount?: number;
+  sellersCount?: number;
+  pendingRequests?: number;
+  totalCategories?: number;
+  totalBuyers?: number;
   improvement?: {
     models: number,
     users: number,
-    revenue: number
+    revenue: number,
+    sellers: number,
+    pendingRequests: number,
+    buyers: number
   }
 
 }
+
+// export interface advannce{
+//   totalModels?: number;
+//   totalUsers?: number;
+//   totalRevenue?: number;
+//   totalPurchases?: number;
+//   totalLikes?: number;
+//   totalDownloads?: number;
+//   mostPopularModel?: model;
+//   lastMonthRevenue?: number;
+//   lastWeekRevenue?: number;
+//   newModelsLastWeek?: number;
+//   newUsersLastWeek?: number;
+//   moderatorsCount?: number;
+//   sellersCount?: number;
+//   pendingRequests?: number;
+//   totalCategories?: number;
+//   totalBuyers?: number;
+//   improvement?: {
+//     models: number,
+//     users: number,
+//     revenue: number,
+//     sellers: number,
+//     pendingRequests: number,
+//     buyers: number
+//   }
+// }
 
 export interface model2 {
   id?: number;
