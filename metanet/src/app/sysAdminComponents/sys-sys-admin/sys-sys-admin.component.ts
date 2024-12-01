e605ef794c945fe8f6c170d6d54e3f62d59317cd
Isuru Liyanage <126383627+isuru-liyanage@users.noreply.github.com>
2024-12-01 19:14:25 +0530

    sys admin view updated

--- metanet/src/app/sysAdminComponents/sys-sys-admin/sys-sys-admin.component.ts
+++ metanet/src/app/sysAdminComponents/sys-sys-admin/sys-sys-admin.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AdminUserTableComponent} from "../../commonComponents/admin-user-table/admin-user-table.component";
 import {AdminnavbarComponent} from "../../commonComponents/adminnavbar/adminnavbar.component";
 import {SysadminSidebarComponent} from "../../commonComponents/sysadmin-sidebar/sysadmin-sidebar.component";
@@ -6,6 +6,12 @@ import {
   SalesByCategoryPieChartComponent
 } from "../../commonComponents/sales-by-category-pie-chart/sales-by-category-pie-chart.component";
 import {WeeklyRevenueChartComponent} from "../../commonComponents/weekly-revenue-chart/weekly-revenue-chart.component";
+import {UserTableViewonlyComponent} from "../../commonComponents/user-table-viewonly/user-table-viewonly.component";
+import {UserList,error} from "../../domain/models";
+import {BASE_url} from "../../app.config";
+import {HttpClient} from "@angular/common/http";
+import {ToastService} from "angular-toastify";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-sys-sys-admin',
@@ -15,11 +21,35 @@ import {WeeklyRevenueChartComponent} from "../../commonComponents/weekly-revenue
     AdminnavbarComponent,
     SysadminSidebarComponent,
     SalesByCategoryPieChartComponent,
-    WeeklyRevenueChartComponent
+    WeeklyRevenueChartComponent,
+    UserTableViewonlyComponent
   ],
   templateUrl: './sys-sys-admin.component.html',
   styleUrl: './sys-sys-admin.component.css'
 })
-export class SysSysAdminComponent {
+export class SysSysAdminComponent implements OnInit{
+  data:UserList={users:[],total2:0,activeUsers:0,deactivatedUsers:0};
+  constructor(private http: HttpClient,private _toastService: ToastService,private fb: FormBuilder){}
+  ngOnInit() {
+    this.getUsers()
+  }
+  getUsers() {
+    this.http.get<UserList>(BASE_url+'/auth/users?role=sysadmin',{withCredentials:true}).subscribe({
+      next: (data: any) => {
+        this.data = data;
+        console.log(data);
+
+      },
+      error: (error: { error:error }) => {
+        if (error.error.statusCode == 401) {
+          this._toastService.error('You are not authorized to view this page');
+          console.log(error.error.statusCode);
+        }
+        // console.log(error);
+      }
+
+    });
+  }
+
 
 }
