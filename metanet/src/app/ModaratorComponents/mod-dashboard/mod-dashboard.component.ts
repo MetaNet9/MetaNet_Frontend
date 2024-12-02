37abcae04188200015ccff748599771ffebc2664
Isuru Liyanage <126383627+isuru-liyanage@users.noreply.github.com>
2024-12-02 15:00:20 +0530

    moderator dash updated not connected to backend

--- metanet/src/app/ModaratorComponents/mod-dashboard/mod-dashboard.component.ts
+++ metanet/src/app/ModaratorComponents/mod-dashboard/mod-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {
     AdminCardWithChartComponent
 } from "../../commonComponents/admin-card-with-chart/admin-card-with-chart.component";
@@ -10,6 +10,10 @@ import {
 import {TopModelsTableComponent} from "../../commonComponents/top-models-table/top-models-table.component";
 import {WeeklyRevenueChartComponent} from "../../commonComponents/weekly-revenue-chart/weekly-revenue-chart.component";
 import {ModeratorSidebarComponent} from "../../commonComponents/moderator-sidebar/moderator-sidebar.component";
+import {
+  ContributionsModeratorPieChartComponent
+} from "../../commonComponents/contributions-moderator-pie-chart/contributions-moderator-pie-chart.component";
+import {moderatorContributions} from "../../domain/models";
 
 @Component({
   selector: 'app-mod-dashboard',
@@ -21,11 +25,23 @@ import {ModeratorSidebarComponent} from "../../commonComponents/moderator-sideba
     SalesByCategoryPieChartComponent,
     TopModelsTableComponent,
     WeeklyRevenueChartComponent,
-    ModeratorSidebarComponent
+    ModeratorSidebarComponent,
+    ContributionsModeratorPieChartComponent
   ],
   templateUrl: './mod-dashboard.component.html',
   styleUrl: './mod-dashboard.component.css'
 })
-export class ModDashboardComponent {
+export class ModDashboardComponent implements OnInit{
+  constructor() { }
 
+  sampleContributions!:moderatorContributions[]
+  ngOnInit() {
+    this.sampleContributions = [
+      {user: "user1", contributions: 10},
+      {user: "user2", contributions: 20},
+      {user: "user3", contributions: 30},
+      {user: "user4", contributions: 40},
+      {user: "user5", contributions: 50}
+      ]
+  }
 }
